675fed7948d4943a18cdc8d85ea9d49949fe63cf
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-07-28 12:12:37 +0100

    Done with the assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+let a = 2000
+let b = 100
+
+function divide(a,b) {
+   return a/b
+}
+
+divide(a, b)
+
+const square = (num) => num * num
+const add = (num, num2) => {
+    return num + num2;
+}
+
